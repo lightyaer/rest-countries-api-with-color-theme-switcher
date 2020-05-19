687993aae7fd5430b29f2c6002eca3a708fc51d2
lightyaer <mmnyed@gmail.com>
2020-05-19 14:23:38 +0530

    Setup routes & components for CountryList and CountryDetail

---
+++ src/country/list/CountryListView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'CountryListView'
+};
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,38 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import CountryView from '../country/CountryView';
+import CountryListView from '../country/list/CountryListView';
+import CountryDetailView from '../country/detail/CountryDetailView';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'Home',
+    redirect: { name: 'country' }
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/country',
+    component: CountryView,
+    children: [
+      { path: '', name: 'country', component: CountryListView },
+      {
+        path: ':countryId',
+        name: 'countryDetail',
+        component: CountryDetailView
+      }
+    ]
   }
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // }
 ];
 
 const router = new VueRouter({
