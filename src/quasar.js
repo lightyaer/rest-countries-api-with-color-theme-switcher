f8f009c4ddd0e958ac3ca9b1d4b27cf75b49a962
lightyaer <mmnyed@gmail.com>
2020-05-19 14:20:38 +0530

    Fix formatting for default files

--- src/quasar.js
+++ src/quasar.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
+import Vue from 'vue';
 
-import "./styles/quasar.scss";
-import "quasar/dist/quasar.ie.polyfills";
-import "@quasar/extras/material-icons/material-icons.css";
-import { Quasar } from "quasar";
+import './styles/quasar.scss';
+import 'quasar/dist/quasar.ie.polyfills';
+import '@quasar/extras/material-icons/material-icons.css';
+import { Quasar } from 'quasar';
 
 Vue.use(Quasar, {
   config: {},
